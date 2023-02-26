e17edbf19826d156f50316a740bf2461f3dcf915
Richard Hao <richard@digitalstar.tech>
2023-02-27 00:16:45 +0800

    feat: add dotenv and goerli network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { GOERLI_URL, ACCOUNT_PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    }
+  },
 };
