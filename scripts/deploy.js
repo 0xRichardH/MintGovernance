289e0e8511d36e55c3681a493f17e57fbb3d93e1
Richard Hao <richard@digitalstar.tech>
2023-02-27 00:34:23 +0800

    feat: deploy the governor contract and token contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  const [owner] = await ethers.getSigners();
   const transactionCount = await owner.getTransactionCount();
 
   // gets the address of the token before it is deployed
@@ -20,6 +21,8 @@ async function main() {
     `Token deployed to ${token.address}`
   );
 }
+
+// Governor deployed to 0x7F244f7b047366B927a3ad6FFa0D5a7B055ee0b7 Token deployed to 0x6B49461f92FA9d9F70b36De2B4ec635971B86607
 
 main().catch((error) => {
   console.error(error);
